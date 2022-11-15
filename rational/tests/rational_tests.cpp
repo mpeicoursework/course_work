d018d5af4d1a33c62dba197d880b4f901551eb39
gleb032 <fandeev.business@gmail.com>
2022-11-15 13:09:39 +0300

    tests fixed

--- rational/tests/rational_tests.cpp
+++ rational/tests/rational_tests.cpp
@@ -4,58 +4,51 @@
 #include <iostream>
 
 TEST(rational_tests, equality_operator_test) {
-    rational::rational r1("25.49");
-    rational::rational r2("14.492");
+    rational::rational r1(1,3);
+    rational::rational r2(1,4);
     ASSERT_TRUE(r2 < r1);
     ASSERT_TRUE(r1 > r2);
     
-    rational::rational r3("25.49");
+    rational::rational r3(1,3);
     ASSERT_TRUE(r1 == r3);
     ASSERT_TRUE(r1 >= r3);
 
-    rational::rational r4("-25.10314");
-    rational::rational r5("-50.21");
+    rational::rational r4(-5,11);
+    rational::rational r5(-17, 10);
     ASSERT_TRUE(r5 < r4);
 }
 
 TEST(rational_tests, adding_operator_test) {
-    rational::rational r1("25.49");
-    rational::rational r2("14.492");
+    rational::rational r1(1,6);
+    rational::rational r2(2,6);
     rational::rational r3 = r1 + r2;
-    rational::rational expected1("39.982");
+    rational::rational expected1(1,2);
     ASSERT_EQ(r3, expected1);
 
-    rational::rational r4("0.52");
-    rational::rational r5("0.79");
+    rational::rational r4(1,3);
+    rational::rational r5(1,2);
     rational::rational r6 = r4 + r5;
-    rational::rational expected2("1.31");
+    rational::rational expected2(5,6);
     ASSERT_EQ(r6, expected2);
 
-    rational::rational r7("-0.52");
-    rational::rational r8("-1.52");
+    rational::rational r7(-1,2);
+    rational::rational r8(-1,2);
     rational::rational r9 = r7 + r8;
-    rational::rational expected3("-2.04");
+    rational::rational expected3(-1,1);
     ASSERT_EQ(r9, expected3);
-
-    rational::rational r10("-0.52");
-    rational::rational r11("1.52");
-    rational::rational r12 = r10 + r11;
-    rational::rational expected4("1.00");
-    ASSERT_EQ(r12, expected4);
-
-    rational::rational r13("-1.52");
-    rational::rational r14("0.49");
-    rational::rational r15 = r13 + r14;
-    std::cout << r15 << std::endl;
-    rational::rational expected5("-1.03");
-    ASSERT_EQ(r15, expected5);
 }
 
 TEST(rational_tests, diff_operator_tests) {
-    rational::rational r1("25.49");
-    rational::rational r2("14.492");
-    rational::rational r3 = r1 - r2;
-    rational::rational expected("10.998");
-    ASSERT_EQ(r3, expected);
+    rational::rational r10(2,1);
+    rational::rational r11(1,2);
+    rational::rational r12 = r10 - r11;
+    rational::rational expected4(3,2);
+    ASSERT_EQ(r12, expected4);
+
+    rational::rational r13(-3,2);
+    rational::rational r14(1,2);
+    rational::rational r15 = r13 - r14;
+    rational::rational expected5(-2,1);
+    ASSERT_EQ(r15, expected5);
 }
 
--- utils/tests/utils_tests.cpp
+++ utils/tests/utils_tests.cpp
@@ -2,40 +2,30 @@
 
 #include <utils.h>
 
-TEST(utils_tests, test_function_convert_string_to_vector) {
-    std::vector<int> expected = {3, 2, 1};
-    ASSERT_EQ(utils::make_vector_from_string("123"), expected);
+TEST(utils_tests, test_gcd_function) {
+    long long expected1 = 2;
+    long long gcd1 = utils::gcd(4,2);
+    ASSERT_EQ(gcd1, expected1);
+
+    long long expected2 = 1;
+    long long gcd2 = utils::gcd(13,7);
+    ASSERT_EQ(gcd2, expected2);
+
+    long long expected3 = 5;
+    long long gcd3 = utils::gcd(15,10);
+    ASSERT_EQ(gcd3, expected3);
 }
 
-TEST(utils_tests, test_function_remove_leading_zeros) {
-    std::vector<int> expected = {1, 2, 3};
-    std::vector<int> input = {1, 2, 3, 0, 0, 0};
-    utils::remove_leading_zeros(input);
-    ASSERT_EQ(input.size(), expected.size());
-    for (size_t i = 0; i < expected.size(); ++i) {
-        EXPECT_EQ(input[i], expected[i]);
-    }
-}
+TEST(utils_tests, test_lcm_function) {
+    long long expected1 = 4;
+    long long gcd1 = utils::gcd(4,2);
+    ASSERT_EQ(gcd1, expected1);
 
-TEST(utils_tests, test_function_add_trailing_zeros) {
-    std::vector<int> expected = {0, 0, 0, 1, 2, 3};
-    std::vector<int> input = {1, 2, 3};
-    utils::add_trailing_zeros(input, 3);
-    ASSERT_EQ(input.size(), expected.size());
-    for (size_t i = 0; i < expected.size(); ++i) {
-        EXPECT_EQ(input[i], expected[i]);
-    }
-}
+    long long expected2 = 13 * 7;
+    long long gcd2 = utils::gcd(13,7);
+    ASSERT_EQ(gcd2, expected2);
 
-TEST(utils_tests, test_compare_vectors) {
-    std::vector<int> v1 = {1, 2, 3};
-    std::vector<int> v2 = {2, 3, 4};
-    ASSERT_TRUE(utils::compare_two_positive_vectors(v1, v2, [](int a, int b){
-        return a < b;
-    }));
-
-    std::vector<int> v3 = {5, 4, 3};
-    ASSERT_TRUE(utils::compare_two_positive_vectors(v3, v1, [](int a, int b){
-        return a > b;
-    }));
+    long long expected3 = 30;
+    long long gcd3 = utils::gcd(15,10);
+    ASSERT_EQ(gcd3, expected3);
 }
