796a31eb9bf6fd9b9d33b51e5090da61e11949d0
gleb032 <fandeev.business@gmail.com>
2022-12-12 04:14:48 +0300

    task power + big_int cmake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <rational.h>
+#include <tasks.h>
 
 #include <iostream>
 
 int main() {
+    rational::rational r("11/2");
+    std::cout << r;
     return 0;
 }
---
+++ tasks/tasks.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <rational.h>
+
+namespace tasks {
+
+}
