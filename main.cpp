d7cc201290efb0c360f9779ff7e5333cfc2ee52c
maxim partin <max.physmath.05@gmail.com>
2022-12-12 16:20:55 +0300

    add LU solution method

--- main.cpp
+++ main.cpp
@@ -4,10 +4,50 @@
 #include <iostream>
 
 int main() {
-    rational::rational r1("11/2");
+     rational::rational r1("11/2");
     rational::rational r2("11/2");
     rational::rational r = r1 + r2;
     std::cout << r << std::endl;
     std::cout << "1/4 = " << static_cast<double>(1) / static_cast<double>(4) << std::endl; 
+
+    std::vector<std::vector<double>> matrix(3);
+    matrix[0].push_back(9);
+    matrix[0].push_back(0);
+    matrix[0].push_back(-11);
+    matrix[1].push_back(-9);
+    matrix[1].push_back(8);
+    matrix[1].push_back(6);
+    matrix[2].push_back(18);
+    matrix[2].push_back(-80);
+    matrix[2].push_back(32);
+    auto [L, U] = tasks::LUDecomposition(matrix, static_cast<double>(0), static_cast<double>(1));
+    std::cout << "LU finish" << std::endl;
+    std::cout << "L[0][0] = " << L[0][0] << std::endl;
+    std::cout << "L[0][1] = " << L[0][1] << std::endl;
+    std::cout << "L[0][2] = " << L[0][2] << std::endl;
+    std::cout << "L[1][0] = " << L[1][0] << std::endl;
+    std::cout << "L[1][1] = " << L[1][1] << std::endl;
+    std::cout << "L[1][2] = " << L[1][2] << std::endl;
+    std::cout << "L[2][0] = " << L[2][0] << std::endl;
+    std::cout << "L[2][1] = " << L[2][1] << std::endl;
+    std::cout << "L[2][2] = " << L[2][2] << std::endl;
+
+    std::cout << "LU finish" << std::endl;
+    std::cout << "U[0][0] = " << U[0][0] << std::endl;
+    std::cout << "U[0][1] = " << U[0][1] << std::endl;
+    std::cout << "U[0][2] = " << U[0][2] << std::endl;
+    std::cout << "U[1][0] = " << U[1][0] << std::endl;
+    std::cout << "U[1][1] = " << U[1][1] << std::endl;
+    std::cout << "U[1][2] = " << U[1][2] << std::endl;
+    std::cout << "U[2][0] = " << U[2][0] << std::endl;
+    std::cout << "U[2][1] = " << U[2][1] << std::endl;
+    std::cout << "U[2][2] = " << U[2][2] << std::endl;
+    std::vector<double> b = {-13, 11, 2};
+
+    std::vector<double> x = tasks::LUSolution(L, U, b);
+    std::cout << "x[0] = " << x[0] << std::endl;
+    std::cout << "x[1] = " << x[1] << std::endl;
+    std::cout << "x[2] = " << x[2] << std::endl;
+
     return 0;
 }
--- tasks/tasks.h
+++ tasks/tasks.h
@@ -8,4 +8,59 @@ namespace tasks {
 
     /* Извлечение корня 'root' степени из basis */
     rational::rational root(rational::rational basis, int root);
+
+
+    template<typename T>
+    std::tuple<std::vector<std::vector<T>>, std::vector<std::vector<T>>> 
+    LUDecomposition(const std::vector<std::vector<T>>& matrix, T zero, T one) {
+        size_t count_rows = matrix[0].size();
+        std::vector<std::vector<T>> U = matrix;
+        std::vector<std::vector<T>> L(count_rows, std::vector<T>(count_rows));
+        for (size_t i = 0; i < count_rows; ++i) {
+            for (size_t j = 0; j < count_rows; ++j) {
+                if (i == j) {
+                    L[i][j] = one;
+                } else {
+                    L[i][j] = zero;
+                }
+            }
+        }
+
+        for (size_t k = 0; k < count_rows - 1; ++k) {
+            for (size_t i = k + 1; i < count_rows; ++i) {
+                T mu_i_k = U[i][k] / U[k][k];
+                L[i][k] = mu_i_k;
+                U[i][k] -= U[k][k] * mu_i_k;
+                for (size_t j = k + 1; j < count_rows; ++j) {
+                    U[i][j] -= (mu_i_k * U[k][j]);
+                }
+            }
+        }
+        return std::make_tuple(L, U);
+    }
+
+    // LU solution the system of linear algebraic equations
+    template<typename T> 
+    std::vector<T> LUSolution(const std::vector<std::vector<T>>& L, const std::vector<std::vector<T>>& U, const std::vector<T>& b) {
+        size_t count_rows = L[0].size();
+        std::vector<T> y(count_rows);
+        y[0] = b[0];
+        for (size_t i = 1; i < count_rows; ++i) {
+            y[i] = b[i];
+            for (size_t j = 0; j < i; ++j) {
+                y[i] -= L[i][j] * y[j];
+            }
+        }
+
+        std::vector<T> x(count_rows);
+        x[count_rows - 1] = y[count_rows - 1] / U[count_rows - 1][count_rows - 1];
+        for (int64_t i = count_rows - 2; i > -1; --i) {
+            T cur = y[i];
+            for (int64_t j = count_rows - 1; j > i; --j) {
+                cur -= U[i][j] * x[j];
+            }
+            x[i] = cur / U[i][i];
+        }
+        return x;
+    }
 }
